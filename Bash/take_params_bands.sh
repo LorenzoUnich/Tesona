bb8687fdea079d4f4d7843ed197b690a8e4701f4
Science Carrot <80289416+LorenzoUnich@users.noreply.github.com>
2025-07-24 13:22:52 +0200

    Add files via upload

---
+++ Bash/take_params_bands.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Lista di cartelle dove cercare
+cartelle=(
+  "1006"
+  "0602"
+  "0202"
+  "0206"
+)
+rm -f extracted_summary_bands.txt  # elimina il file se esiste
+# Intestazione della tabella
+echo -e "i\tss90\tdiscovery\ttotal_signal\ttotal_noise\tfolder" > extracted_summary_bands.txt
+
+# Loop su ciascuna cartella
+for dir in "${cartelle[@]}"; do
+  for file in "$dir"/o__*_100000_PE_with_multiprocessing_aysn_different_chunksize_four_less_GIGA_200000PE_SECOND_2PI.txt; do
+    # Controlla che il file esista (evita errori se non matcha nulla)
+    [[ -e "$file" ]] || continue
+
+    filename=$(basename "$file")
+    i=$(echo "$filename" | grep -oP '(?<=o__)\d+')
+
+    ss90=$(grep -m1 "ss90:" "$file" | awk '{print $2}')
+    discovery=$(grep -m1 "discovery:" "$file" | awk '{print $2}')
+    total_signal=$(grep -m1 "TOTAL signal:" "$file" | awk '{print $3}')
+    total_noise=$(grep -m1 "TOTAL background:" "$file" | awk '{print $3}')
+    echo -e "$i\t$ss90\t$discovery\t$total_signal\t$total_noise\t$dir"
+  done
+done >> extracted_summary_bands.txt
