cd6e82f56e07d9b1782b93f3ad7d7a394c72f4b4
Science Carrot <80289416+LorenzoUnich@users.noreply.github.com>
2025-09-13 21:11:09 +0200

    Create take_root_nb.sh

---
+++ Bash/take_root_nb.sh
@@ -0,0 +1,18 @@
+#script to take the root files of the analysis without any band 
+
+remote_base="ulorenzo@cca.in2p3.fr:/sps/km3net/users/lunich/binned/arca-ps-aart_update_bands/scripts/declination_arca_21"
+
+files=(
+  "Source_cand_0_first_declination_histograms_without_2pi.root"
+  "Source_cand_1_second_declination_histograms_without_2pi.root"
+  "Source_cand_2_third_declination_histograms_without_2pi.root"
+  "Source_cand_3_four_declination_histograms_without_2pi.root"
+  "Source_cand_4_fifth_declination_histograms_without_2pi.root"
+  "Source_cand_5_six_declination_histograms_without_2pi.root"
+  "Source_cand_6_seven_declination_histograms_without_2pi.root"
+  "Source_cand_7_eight_declination_histograms_without_2pi.root"
+)
+
+for file in "${files[@]}"; do
+  scp "$remote_base/$file" .
+done
